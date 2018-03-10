e47f436e301c8f2930beca0ce749fbea039a3a21
William Witt <william.witt@mimacom.com>
2018-03-10 12:13:22 -0600

    initial commit

---
+++ shape-service/src/main/java/com/mimacom/shapeservice/ShapeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mimacom.shapeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeServiceApplication.class, args);
+	}
+}
---
+++ sign-service/src/main/java/com/mimacom/signservice/SignServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mimacom.signservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignServiceApplication.class, args);
+	}
+}
