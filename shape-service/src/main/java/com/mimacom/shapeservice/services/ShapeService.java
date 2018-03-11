fa3cd3d8cd5eb9bb941102a49f8cf1e871d6e286
William Witt <william.witt@mimacom.com>
2018-03-11 14:19:05 -0500

    fix typo

--- shape-service/src/main/java/com/mimacom/shapeservice/services/ShapeService.java
+++ shape-service/src/main/java/com/mimacom/shapeservice/services/ShapeService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShapeService {
 
-    public Shape getTriangle(Double height, Double width) {
+    public Shape getTriangle(Double length, Double width) {
         Shape shape = Shape.builder()
-                .area(height * width / 2)
+                .area(length * width / 2)
                 .build();
         return shape;
     }
